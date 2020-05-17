74e9aa73f048714be211bc97e29fd5c9fe9393d7
Pearly-choong <choongpl@gmail.com>
2020-05-17 19:17:03 +1200

    woocommerce overrride

--- functions.php
+++ functions.php
@@ -3,7 +3,7 @@
 //load stylesheets
 function load_css(){
     //bootstrap
-    wp_register_style('bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css', array(), false, 'all');
+    wp_register_style('bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css', array(), '4.5.0', 'all');
     wp_enqueue_style('bootstrap');
 
     //own css style sheet
@@ -19,9 +19,10 @@ function load_js()
 {
 		wp_enqueue_script('jquery');
 
-		wp_register_script('bootstrap', get_template_directory_uri() . '/js/bootstrap.min.js', 'jquery', false, true);
+		wp_register_script('bootstrap', get_template_directory_uri() . '/js/bootstrap.min.js', 'jquery', '4.5.0', true);
 		wp_enqueue_script('bootstrap');
-
+    wp_register_script('js', get_template_directory_uri() . '/js/script.js', array('jquery'), false, true);
+		wp_enqueue_script('js');
 
 }
 add_action('wp_enqueue_scripts', 'load_js');
---
+++ js/script.js
@@ -0,0 +1,10 @@
+console.log('js');
+
+$=jQuery;
+
+$(document).ready(function(){
+    //  $('body').click(function(){
+    // $('body').css('color', 'red');
+    // });
+
+});
